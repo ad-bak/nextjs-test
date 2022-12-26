70e38ed298644fa4599be86898e46db7fffb3364
Adnan <adbak.dev@gmail.com>
2022-12-27 00:43:01 +0100

    added layout

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,14 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
+
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
